868fcee4f3f53b4741930c84061301b46d47d686
SenLin <wumulaozu@gmail.com>
2024-06-07 10:18:52 +0800

    taskCard

--- lib/pages/home/tabContent/taskCard.dart
+++ lib/pages/home/tabContent/taskCard.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:marewood_client/models/task.dart';
 
+class Status extends StatelessWidget{
+  const Status({super.key, required this.status});
+
+  final int status;
+
+  @override
+  Widget build(BuildContext context) {
+    switch (status) {
+      case 0:
+        return const Icon(Icons.panorama_fish_eye_outlined,color: Colors.grey);
+      case 1:
+        return const SizedBox(width: 18.0, height: 18.0, child: CircularProgressIndicator(strokeWidth: 2),);
+      case 2:
+        return const Icon(Icons.remove_circle_outline,color: Colors.redAccent);
+      case 3:
+        return const Icon(Icons.check_circle_outline,color: Colors.green);
+      default:
+        return const Icon(Icons.panorama_fish_eye_outlined,color: Colors.grey);
+    }
+  }
+
+}
+
+
+
 class TaskCard extends StatelessWidget{
   const TaskCard({super.key, required this.task});
 
   final Task task;
+
+  final bodyText =  const TextStyle(color:  Colors.grey);
 
   @override
   Widget build(BuildContext context) {
@@ -12,38 +39,55 @@ class TaskCard extends StatelessWidget{
       margin: const EdgeInsets.symmetric(vertical: 9,horizontal:
       18),
       child: Padding(
-          padding: const EdgeInsets.all(12),
+          padding: const EdgeInsets.only(left: 12,right: 12,top: 18),
           child: Column(
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(task.name),
-                  Text('Status:${task.status.toString()}'),
+                  Expanded(
+                      child:Text(
+                          '${task.tag}${task.name}',
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(color: Color.fromARGB(255, 3, 7, 18))),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Status(status: task.status),
+                  ),
+                  // Text('Status:${task.status.toString()}'),
                 ],
               ),
+              const Divider(color: Color.fromARGB(255, 229, 231, 235)),
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 10),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text('Hash:${task.commitHash}'),
-                    Text('BuildDir:${task.buildDir}'),
-                    Text('RunTotal:${task.runTotal}'),
+                    Text('Branch:${task.branch}',style: bodyText),
                   ],
                 ),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text('Branch:${task.branch}'),
-                  Text('BuildCmd:${task.buildCommand}'),
+                  Text('Hash:${task.commitHash}',style: bodyText),
+                  Text('RunTotal:${task.runTotal}',style: bodyText),
                 ],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Text('ID:${task.id.toString()}'),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 4),
+                        child: Text('#${task.id.toString()}',style: const TextStyle(color:  Colors.grey)),
+                      ),
+                      if(task.private)const Icon(Icons.lock_outline,color: Colors.green,size: 16),
+                    ],
+                  ),
                   Row(
                     children: [
                       IconButton(
@@ -52,6 +96,10 @@ class TaskCard extends StatelessWidget{
                       ),
                       IconButton(
                         icon: const Icon(Icons.save_alt),
+                        onPressed: () {},
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.more_horiz),
                         onPressed: () {},
                       )
                     ],
